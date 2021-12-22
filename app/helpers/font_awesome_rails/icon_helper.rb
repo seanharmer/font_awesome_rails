ac405a85e0ade9c4e6d70513c3d4024249ce577a
Sean Harmer <sean.harmer@kdab.com>
2021-12-22 13:57:47 +0000

    Pass in options

--- app/helpers/font_awesome_rails/icon_helper.rb
+++ app/helpers/font_awesome_rails/icon_helper.rb
@@ -2,7 +2,7 @@ require 'font_awesome_rails/fa_icon_util'
 
 module FontAwesomeRails
   module IconHelper
-    def fa_icon(icon)
+    def fa_icon(icon, options = {})
       FontAwesomeRails::FaIconUtil.new(icon, options).tag
     end
   end
